e7de9237571ece7e34c9ad2cbd4c563f26ed24c6
Yurii Denkovych <yd2004@ukr.net>
2022-05-23 18:14:16 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const listGallery = document.querySelector('.gallery');
+const linkImageAll = listGallery.querySelectorAll(".gallery__item");
 
+const markup = galleryItems.map((image)=>
+`<a class="gallery__item" href="${image.original}">
+    <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+    title="${image.description}"
+    />
+</a>` ).join('');
+listGallery.insertAdjacentHTML( 'beforeend', markup);
+
+listGallery.addEventListener('click', onClickImage);
+
+function onClickImage(event){
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {  captionDelay: 250,})
+    
+    
+}
+
+console.log(galleryItems);
 console.log(galleryItems);
