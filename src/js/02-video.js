37f03b79e8cae17b84f6f3726b4cb8a09f36a372
Yurii Denkovych <yd2004@ukr.net>
2022-05-24 20:04:23 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,40 @@ const video = document.querySelector("#vimeo-player");
 console.log(video);
 const player = new Vimeo.Player(video);
 console.log(player);
-player.on('play', function() {
-    console.log('played the video!');
-});
+//player.on('play', function() {
+    //console.log('played the video!');
+//});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-let currentTime = '';
-console.log(currentTime);
+//player.getVideoTitle().then(function(title) {
+    //console.log('title:', title);
+//});
+
+
+//console.log(dataSec);
+let t = [];
+console.log(t);
 player.on('timeupdate', function(data) {
-   let currentTime = localStorage.setItem("videoplayer-current-time", `${data}`);
-   console.log(currentTime);
-   return currentTime;
+   // console.log(data);
+   if(data !== 0){
+    console.log(data.seconds);
+     
+   }
+   
     
+   //console.log(data);
+   
+    // seconds = the actual time that the player seeked to
+
 });
+player.on('pause', function(data){
+    console.log(data);
+});
+localStorage.setItem("videoplayer-current-time", t.seconds);
+    let time = localStorage.getItem('videoplayer-current-time');
+    console.log(time);
+player.setCurrentTime(10);
+
+//console.log(data);
+//localStorage.setItem("videoplayer-current-time", data.seconds);
+//let time = localStorage.getItem('videoplayer-current-time');
+//console.log(time);
