a9d8f4e92358843852bbb8aed1304afe2e7a9719
Yurii Denkovych <yd2004@ukr.net>
2022-05-24 16:55:14 +0300

    Update package-lock.json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const video = document.querySelector("#vimeo-player");
+console.log(video);
+const player = new Vimeo.Player(video);
+console.log(player);
+player.on('play', function() {
+    console.log('played the video!');
+});
 
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+let currentTime = '';
+console.log(currentTime);
+player.on('timeupdate', function(data) {
+   let currentTime = localStorage.setItem("videoplayer-current-time", `${data}`);
+   console.log(currentTime);
+   return currentTime;
+    
+});
