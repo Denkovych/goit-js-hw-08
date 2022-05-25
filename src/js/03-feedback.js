9fa3c4e9329b377218a45084dba326ee9dce6344
Yurii Denkovych <yd2004@ukr.net>
2022-05-25 18:52:24 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,42 @@
 const feedbackForm =document.querySelector(".feedback-form");
 console.log(feedbackForm);
-const emailInput = feedbackForm.querySelector('[name = email]');
+const emailInput = feedbackForm.querySelector('[type = "email"]');
 console.log(emailInput);
-const messageInput = feedbackForm.querySelector('[name = message]');
+const messageInput = feedbackForm.querySelector('[name = "message"]');
 console.log(messageInput);
-const btnSubmit = feedbackForm.querySelector('[type = submit]');
-console.log(btnSubmit)
 var throttle = require('lodash.throttle');
 
-btnSubmit.addEventListener('submit', pushBtnSubmit);
+feedbackForm.addEventListener('submit', pushBtnSubmit);
 emailInput.addEventListener('input', throttle(inputEmail, 500) );
 messageInput.addEventListener('input', throttle(inputMessage, 500));
 
-function pushBtnSubmit(evt){
-    evt.preventDefault(); 
-    let loginInfo = {};
-    if (emailInput.value !== '' && messageInput.value !== '')
-    {loginInfo.email = emailInput.value;
-    loginInfo.message = messageInput.value;   
-    console.log(loginInfo);
-    feedbackForm.reset();
-     emailInput.reset()
-    messageInput.reset()}
-}
-
-
 function inputEmail(evt){
     evt.preventDefault();
-    console.log(evt.target.value);
     email = evt.target.value;
     localStorage.setItem('email', email)};
 
     let email = localStorage.getItem('email');
     emailInput.value = email;
 
-function inputMessage(evt){
-    evt.preventDefault();
-    console.log(evt.target.value);
-    message = evt.target.value;
+function inputMessage(evt){ 
+    evt.preventDefault(); 
+    message = evt.target.value; 
     localStorage.setItem('message', message)};
-
-    let message = localStorage.getItem('message');
+    let message = localStorage.getItem('message'); 
     messageInput.value = message;
-    
-    console.log(messageInput.target.value);
+
+function pushBtnSubmit (evt) {
+    evt.preventDefault(); 
+    let loginInfo = {};
+
+    if(emailInput.value === "" || messageInput.value ===""){
+        alert('Потрібно заповнити всі поля');}
+    else if (emailInput.value !== '' && messageInput.value !== ''){
+        loginInfo.email = `${emailInput.value}`;
+        loginInfo.message = `${messageInput.value}`; 
+        console.log(loginInfo);
+
+        feedbackForm.reset(); 
+        localStorage.removeItem('message')
+        localStorage.removeItem('email')}};
+        
