b3a284075b919985879a0f2bc330350cc285a7b0
Yurii Denkovych <yd2004@ukr.net>
2022-05-25 13:24:48 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,14 @@
 const video = document.querySelector("#vimeo-player");
 const player = new Vimeo.Player(video);
+var throttle = require('lodash.throttle');
 
-player.on('timeupdate', saveCarrentTime);
-
+player.on('timeupdate', throttle(saveCarrentTime, 1000));
+ 
 function saveCarrentTime (data){
     localStorage.setItem("videoplayer-current-time", data.seconds);
-    console.log(data.seconds);
-    video.reset();
-};
+    console.log(data.seconds);};
 
-    let time = localStorage.getItem('videoplayer-current-time');
-    console.log(time);
-    player.setCurrentTime(throttle(time, 1000));
+let time = localStorage.getItem('videoplayer-current-time');
+console.log(time);
+player.setCurrentTime(time);
 
