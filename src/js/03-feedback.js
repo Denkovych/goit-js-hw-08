a1b720fa157cca73337d0142c0ae38f1e3e9e1e7
Yurii Denkovych <yd2004@ukr.net>
2022-05-26 11:23:05 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,27 +14,25 @@ function saveForm(event) {
     event.preventDefault()
     formData.email = emailInput.value;
     formData.message = messageInput.value; 
-    
+
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));}
 
 function inputSaveForm(){
     const saveFormData = localStorage.getItem("feedback-form-state");
     const parsedSaveFormData = JSON.parse(saveFormData);
-    
+    if (parsedSaveFormData){ 
     emailInput.value = parsedSaveFormData.email;
-    messageInput.value = parsedSaveFormData.message;
-    }
+    messageInput.value = parsedSaveFormData.message;}}
 inputSaveForm();   
 
 function pushBtnSubmit (evt) {
     evt.preventDefault(); 
     let loginInfo = {};
 
-    loginInfo.email = `${emailInput.value}`;
-    loginInfo.message = `${messageInput.value}`; 
+    loginInfo.email = emailInput.value;
+    loginInfo.message = messageInput.value; 
     console.log(loginInfo);
 
     feedbackForm.reset(); 
-    localStorage.removeItem('message')
-    localStorage.removeItem('email')};
+    localStorage.removeItem("feedback-form-state")};
         
